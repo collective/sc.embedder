db8ea16592584b8c509acd66b73b3d17ee3e2b58
hvelarde <hector.velarde@gmail.com>
2013-04-02 09:18:19 -0300

    use correct .rst extension on documentation files

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ import os
 
 version = '1.0b3.dev0'
 description = "A multimedia content type for Plone"
-long_description = (open("README.txt").read() + "\n" +
-    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
-    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" +
-    open(os.path.join("docs", "HISTORY.txt")).read())
+long_description = (open("README.rst").read() + "\n" +
+    open(os.path.join("docs", "INSTALL.rst")).read() + "\n" +
+    open(os.path.join("docs", "CREDITS.rst")).read() + "\n" +
+    open(os.path.join("docs", "HISTORY.rst")).read())
 
 setup(name='sc.embedder',
       version=version,
