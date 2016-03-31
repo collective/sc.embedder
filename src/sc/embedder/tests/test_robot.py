3671febb6fe7e6ff902985363e97a211f4aa332e
hvelarde <hector.velarde@gmail.com>
2016-03-31 15:47:14 -0300

    Skip RobotFramework tests in Plone 5
    There are still no valid keywords.

--- src/sc/embedder/tests/test_robot.py
+++ src/sc/embedder/tests/test_robot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from plone.testing import layered
+from sc.embedder.testing import PLONE_VERSION
 from sc.embedder.testing import ROBOT_TESTING
 
 import os
@@ -10,6 +11,10 @@ import unittest
 dirname = os.path.dirname(__file__)
 files = os.listdir(dirname)
 tests = [f for f in files if f.startswith('test_') and f.endswith('.robot')]
+
+# skip RobotFramework tests in Plone 5
+if PLONE_VERSION.startswith('5'):
+    tests = []
 
 
 class Keywords(object):
